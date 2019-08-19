1109bf9f256ce98e1e21d6aa0e7df27b255fee0c
morwen80 <ribesnero80@gmail.com>
2019-08-19 23:55:21 +0200

    added Engines in packageJSON

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,37 +4,7 @@ import {Link} from 'react-router-dom';
 class Navbar extends Component {
   render() {
     return (
-//       <nav className="navbar navbar-expand-sm navbar-dark bg-dark px-sm-5">
-//
-//         <Link to='/'>
-//           <i className="fas fa-hat-wizard navbar-brand"></i> The Wicked Witch
-//         </Link>
-//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//     <span class="navbar-toggler-icon"></span>
-//   </button>
-//
-// <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//
-//         <ul class="navbar-nav mr-auto">
-//
-//           <li className='nav-item ml-5'>
-//             <Link to='/' className="nav-link">
-//               Products
-//             </Link>
-//           </li>
-//         </ul>
-//
-//         <form class="form-inline my-2 my-lg-0">
-//         <Link to="/cart" className="ml-auto">
-//           <button className="btn btn-light cartBtn">
-//             <i className="fas fa-cart-plus" /> my cart</button>
-//         </Link>
-//         </form>
-//         </div>
-//       </nav>
-
-
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-sm-5">
       <Link to='/'>
         <i className="fas fa-hat-wizard navbar-brand"></i> The Wicked Witch
       </Link>
@@ -45,24 +15,20 @@ class Navbar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            
-            <li className="nav-item">
+
+            <li className="nav-item ml-3">
               <Link to='/' className="nav-link">
                 Products
               </Link>
             </li>
-
           </ul>
-          <form className="form-inline my-2 my-lg-0">
+          <form className="form-inline">
+            <Link to="/cart" className="ml-auto">
              <button className="btn btn-light cartBtn"><i className="fas fa-cart-plus" /> my cart</button>
+             </Link>
           </form>
         </div>
       </nav>
-
-
-
-
-
 
     )
   }
